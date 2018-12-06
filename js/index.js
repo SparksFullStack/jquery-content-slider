fc391498f2a6a2866dee09fd85e9bafd73d2fc95
SparksFullStack <csparks@antioch.edu>
2018-12-05 20:39:35 -0800

    basics of the functionality added; now hiding all images except the current active one.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var speed = 500;    // slide transition speed
+    var autoswitch = true; // change value if you don't want the slider to auto switch
+    var autoswitch_speed = 4000;
+
+    // add initial active class
+    $('.slide').first().addClass('active');
+
+    // hide all slides
+    $('.slide').hide();
+
+    // show first slide
+    $('.active').show();
+
+    $('#next').on('click', function(){
+        
+    })
+})
