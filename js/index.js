c7c3bdf402370087538207a6bca6ccb845824042
SparksFullStack <csparks@antioch.edu>
2018-12-05 20:47:25 -0800

    slider working

--- js/index.js
+++ js/index.js
@@ -13,6 +13,28 @@ $(document).ready(function(){
     $('.active').show();
 
     $('#next').on('click', function(){
-        
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    })
+
+    $('#previous').on('click', function(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':first-child')){
+            $('.slide').last().addClass('active');
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
     })
 })
